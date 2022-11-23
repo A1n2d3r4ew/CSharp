2b34ab9ffbfb29427dd6023e7f890f52b76f68e7
Andrew <stav_andrew@mail.ru>
2022-11-23 23:36:59 +0300

    Task HW 1_0

---
+++ 1lesson/HW 1.0/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Напишите первое число: ");
+
+Console.WriteLine("Напишите второе число: ");
+
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+
+if (n1 > n2)
+{
+    Console.WriteLine(n1);
+}
+else if (n1 == n2)
+{
+    Console.WriteLine("ровнозначные");
+}
+else
+    Console.WriteLine(n2);
