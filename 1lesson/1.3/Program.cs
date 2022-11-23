66321090c0742c2083f482e7cfeb8eb92dd78706
Andrew <stav_andrew@mail.ru>
2022-11-23 22:32:14 +0300

    Task 1_3

---
+++ 1lesson/1.3/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Write a numb: ");
+string A = Console.ReadLine();
+int N = int.Parse(A);
+int NegN = - N;
+
+while(NegN <= N)
+{
+    Console.WriteLine($"{NegN} ");
+    NegN +=1;
+}
