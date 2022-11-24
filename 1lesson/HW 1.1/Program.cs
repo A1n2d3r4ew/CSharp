ba6a50c4cab1c6925dd1b1ee34440d7c66cd3716
Andrew <stav_andrew@mail.ru>
2022-11-24 12:57:03 +0300

    Task HW 1_1

---
+++ 1lesson/HW 1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Введите число: ");
+string A = Console.ReadLine();
+int max = int.Parse(A);
+Console.WriteLine("Введите число: ");
+string B = Console.ReadLine();
+int N = int.Parse(B);
+Console.WriteLine("Введите число: ");
+string C = Console.ReadLine();
+int N_1 = int.Parse(C);
+
+if (max > N)
+{
+    if (max > N_1)
+    Console.WriteLine(max);
+}
+else if (N > N_1)
+{
+    Console.WriteLine(N);
+}
+else 
+    Console.WriteLine(N_1);
