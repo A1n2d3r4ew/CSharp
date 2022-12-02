4d84f2922185852fd2d74c7da35bb8f32153841c
Andrew <stav_andrew@mail.ru>
2022-12-03 00:06:10 +0300

    task hw2_0

---
+++ 2lesson/HW 2.0/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход трехзначное 
+//число и на выходе показывает вторую цифру этого числа
+
+
+
+
+string N(int num);
+{
+int count = 1000;
+int max = num;
+while(max < count)
+{
+    if (num % 10 == num / 10)
+        return ($"Вторая цифра числа: {num}");
+    else
+        return ($"Ошибка");
+}
+
+}
+
+Console.WriteLine("Напишите трехзначное число:  ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(N(n));
