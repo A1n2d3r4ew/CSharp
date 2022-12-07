06d34680dc4e9a77aef6af486b98298614539310
Andrew <stav_andrew@mail.ru>
2022-12-07 18:57:34 +0300

    task 3_3

--- 3lesson/3.1/Program.cs
+++ 3lesson/3.1/Program.cs
@@ -1,2 +1,19 @@
-﻿// 
+﻿// Напишите программу, которая по заданному номеру
+// четверти, показывает диапазон возможных координат
+// точек в этой четверти (х и у).
 
+void Quar(string a)
+{
+    if (a == "I")
+        Console.WriteLine("x > 0 and y > 0");
+    else if (a == "II")
+        Console.WriteLine("x < 0 and y > 0");
+    else if (a == "III")
+        Console.WriteLine("x < 0 and y < 0");
+    else if (a == "IV")
+        Console.WriteLine("x > 0 and y < 0");
+    else 
+        Console.WriteLine("error");
+}
+
+Quar(Console.ReadLine());
---
+++ 3lesson/3.2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, уоторая принимает на вход 
+// координаты двух точек и находит расстояние между
+// ними в 2D пространстве.
+// А(3,6);В(2,1) -> 5,09
+// А(7,-5);В(1,-1) -> 7,21
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+
+double Dis(int ax, int ay, int bx, int by)
+{
+    double result;
+    result = Math.Sqrt(Math.Pow(bx-ax,2)+Math.Pow(by-ay,2));
+    return result;
+}
+
+int ax = ReadNumber("Введите aХ: ");
+int ay = ReadNumber("Введите aY: ");
+int bx = ReadNumber("Введите bХ: ");
+int by = ReadNumber("Введите bY: ");
+
+Console.WriteLine(Math.Round(Dis(ax, ay, bx, by),2));
