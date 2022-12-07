6804e0c79b101f6cdb491795809aadfd9bca089f
Andrew <stav_andrew@mail.ru>
2022-12-07 19:14:04 +0300

    task 3_3

--- 3lesson/3.3/Program.cs
+++ 3lesson/3.3/Program.cs
@@ -3,4 +3,27 @@
 // 5 -> 1,4,9,16,25.
 // 2 -> 1,4
 
+void N(int n)
+{
+    int i = 0;
+    double pere;
+    while (i < n)
+    {
+        i ++;
+        if (i == (n))
+        {
+        pere = Math.Pow(i, 2);
+        Console.Write($"{pere}");
+        }
+        else 
+        {
+        pere = Math.Pow(i, 2);
+        Console.Write($"{pere}, "); 
+        }
+    }
 
+}
+
+Console.WriteLine("Введите число: ");
+int chislo = int.Parse(Console.ReadLine());
+N(chislo);
