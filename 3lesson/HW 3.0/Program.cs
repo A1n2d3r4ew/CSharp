39a377047d895146f44dfa4876ce22e714135972
Andrew <stav_andrew@mail.ru>
2022-12-08 21:59:08 +0300

    HW_task 3_0

---
+++ 3lesson/HW 3.0/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+string N(int n)
+{
+   int n1 = 0;
+   int n2 = 0;
+   int n3 = 0;
+   int n4 = 0;
+    while (n < 100000)
+    {
+        if (n > 10000)
+            if ((n1 = n / 10000) == (n2 = n % 10))
+            {
+                if ((n3 = n / 1000 % 10) == (n4 = n % 100 / 10))
+                    return ($"Yes");
+                else 
+                    return ($"No");
+            }
+            else
+                return ($"No");
+        else
+            return ($"error");        
+    }
+    return ($"error");
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write(N(number));
---
+++ 3lesson/HW0000/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+
+
+
+Console.WriteLine("Number ");
+int N = int.Parse(Console.ReadLine());
+int A = N / 1000 % 10;
+Console.WriteLine(A);
