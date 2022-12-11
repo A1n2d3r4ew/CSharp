c9d30a1446694ebcb9ca98eaa512119e7ef00b4d
Andrew <stav_andrew@mail.ru>
2022-12-11 15:21:21 +0300

    HW_task 3_2

---
+++ 3lesson/HW 3.2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+void M(int m)
+{
+    int i = 0;
+    double a;
+    while (i < m)
+    {
+        i ++;
+        if (i == (m))
+        {
+            a = Math.Pow(i, 3);
+            Console.Write($"{a}.");
+        }
+        else 
+        {
+            a = Math.Pow(i, 3);
+            Console.Write($"{a}, ");
+        }
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+M(n);
