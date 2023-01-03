d23e862af2dd9fad3e7bb97bf4c03a0404507615
Andrew <stav_andrew@mail.ru>
2023-01-03 16:18:14 +0300

    HW_task 5_2

--- 5lesson/5_1/Program.cs
+++ 5lesson/5_1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу замена элементов массива:
+// положительные элементы замените на соответствующие
+// отртицательные, и наоборот.
+// [-4,-8,8,2]->[4,8,-8,-2]
+
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+
+    for (int i = 0; i < size ; i ++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int[] Mass(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(-10, 10);
+    return array;
+}
+
+void Massiv(int[] array)
+{
+    
+    for (int i = 0; i < array.Length; i++)
+        array[i] = -array[i];
+           
+} 
+
+int[] array = Mass(int.Parse(Console.ReadLine()));
+PrintArray(array);
+Massiv(array);
+PrintArray(array);
+
---
+++ 5lesson/5_2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте массив. Напишите прогпамму, которая определяет,
+// присутствует ли заданное число в массиве.
+
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+
+    for (int i = 0; i < size ; i ++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int[] Mass(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(-10, 10);
+    return array;
+}
+
+void Num(int[] array, int n)
+{
+    int size = array.Length;
+    for(int i = 0; i < size; i++)
+    {
+        if (n == array[i])
+        {
+            Console.WriteLine("Yes");
+            return;
+        }
+        else
+        {    
+            Console.WriteLine("No");
+            return;
+        }
+    }
+}
+
+int[] array = Mass(int.Parse(Console.ReadLine()));
+PrintArray(array);
+Console.WriteLine("Number  ");
+Num(array, int.Parse(Console.ReadLine()));
