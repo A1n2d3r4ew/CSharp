358bff265ee390f205ca3a7b72a20270e5573a7f
Andrew <stav_andrew@mail.ru>
2023-01-04 16:29:11 +0300

    HW_5_1

---
+++ 5lesson/HW_5_0/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте массив заполненный случайными положительными
+// трехзначными числами. Напишите программу, которая покажет
+// количество четных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] Mass(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(99, 1000);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{array[i]}, ");
+    Console.WriteLine();
+}
+
+int Num(int[] array)
+{
+    int size = array.Length;
+    int count = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    
+    }
+    return count;
+}
+
+int[] array = Mass(int.Parse(Console.ReadLine()));
+PrintArray(array);
+Console.WriteLine(Num(array));
---
+++ 5lesson/HW_5_1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте одномерный массив, заполненный случайными
+// числами.Найдите скмму элементов, стоящих на нечетных позициях.
+
+// [3, 7, 23, 12] -> 26
+// [-4, -6, 4, 67] -> 0
+
+int[] Mass(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(-50, 50);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{array[i]}, ");
+    Console.WriteLine();
+}
+
+int Sum(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i = i + 2)
+        count = count + array[i];
+    return count;
+}
+
+int[] array = Mass(int.Parse(Console.ReadLine()));
+PrintArray(array);
+Console.WriteLine(Sum(array));
