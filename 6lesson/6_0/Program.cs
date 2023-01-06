e68534e88788df737d8501fb39da24f9b3117c45
Andrew <stav_andrew@mail.ru>
2023-01-06 21:21:51 +0300

    task_6

---
+++ 6lesson/6_0/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу которая перевернет одномерный массив
+// (пследний элемент будет на первом месте, а первый - на
+// последнем и т.д)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] Mass(int size, int from, int to)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(from, to);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{array[i]}, ");
+    Console.WriteLine();
+}
+
+void RevMass(int[] array_1)
+{
+    
+    int size = array_1.Length;
+
+    for (int i = 0; i < size / 2; i++)
+        (array_1[i], array_1[size - i - 1]) = (array_1[size - i - 1], array_1[i]);
+        // картеж
+}
+
+
+int[] array_1 = Mass(
+    int.Parse(Console.ReadLine()),
+    int.Parse(Console.ReadLine()),
+    int.Parse(Console.ReadLine()));
+PrintArray(array_1);
+RevMass(array_1);
+PrintArray(array_1);
---
+++ 6lesson/6_4/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая будет создавать копию 
+// заданного массива с помощью поэлементного копирования.
+
+int[,] Mass(int raw, int stolb)
+{
+    int[,] array = new int[raw, stolb];
+
+        for (int i = 0; i < raw; i++)
+            for (int j = 0; j < stolb; j++)
+                array[i, j] = new Random().Next(-50, 50);
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    int stolb = array.GetLength(1);
+    int raw = array.GetLength(0);
+
+    for (int i = 0; i < raw; i++)
+    {    
+        for (int j = 0; j < stolb; j++)
+            Console.Write($"{array[i,j]}, ");
+        Console.WriteLine();
+    }
+}
+
+int[,] Clone(int[,] array)
+{
+    int stolb = array.GetLength(1);
+    int raw = array.GetLength(0);
+    int[,] new_array = new int[raw, stolb];
+
+    for (int i = 0; i < raw; i++)
+    {    
+        for (int j = 0; j < stolb; j++)
+            new_array[i, j] = array[i, j];
+    }
+    return new_array;
+}
+
+int[,] array_1 = Mass(
+    int.Parse(Console.ReadLine()),
+    int.Parse(Console.ReadLine()));
+PrintArray(array_1);
+int[,] array_2 = Clone(array_1);
+Console.WriteLine();
+PrintArray(array_2);
