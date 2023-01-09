e05361f3e70738ffc67621237f4003691f9721a8
Andrew <stav_andrew@mail.ru>
2023-01-09 12:56:23 +0300

    HW_task_6

---
+++ 6lesson/HW_6_0/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько
+// чисел больше 0 ввел пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223 -> 3
+
+int Num()
+{
+    int result = 0;
+    string s;
+
+    while (true)
+    {
+        Console.Write("Press number: ");
+        s = Console.ReadLine();
+
+        if (s == "")
+            return result;
+        else if (int.Parse(s) > 0) 
+            result += 1;
+    
+    }
+    
+}
+
+
+Console.Write(Num());
